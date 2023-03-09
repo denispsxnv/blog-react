515cf87d12bd4d9a206d23dee5c5a385730923cf
denispsxnv <denisazizbekov@cloud.com>
2023-03-09 19:41:32 +0600

    init commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Hello from './components/Hello/Hello';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Hello name="arsen" />
+      <Hello name="Kenan" />
+      <Hello name="Kairat" />
+    </div>
+  );
+}
+
+export default App;
